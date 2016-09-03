d448364517aa56f619cd3fe6c7247c541adec1b8
AntonShumin <AntonShuminBe@gmail.com>
2016-09-03 11:57:55 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pilates Zoya</title>
+        <link rel="stylesheet" type="text/css" href="static/css/main.css">
+    </head>
+    <body>
+        <div id="background_image">
+            <div class="dark_overlay"></div>
+            <img src="static/img/background-3.png">
+        </div>
+
+        <div id="navbar" class="dropshadow_spread">
+            <div class="frame_body">
+                <img src="static/img/logo.png" alt="LOGO">
+            </div>
+        </div>
+
+        <div class="frame_body">
+            <div id="intro_text" class="center_text border_radius_all ">
+                <h1 ">PILATES ANTWEPEN</h1>
+                <p >Pilates lessen voor jong, oud, man of vrouw, iedereen is welkom!</p>
+            </div>
+
+        </div>
+
+
+        <div class="frame_1 dropshadow_spread">
+            <div class="frame_body">
+                <h1 class="center_text sector_title">Hoeveel</h1>
+                <div class="card center_img">
+                    <div class="cards_title">
+                        <h1>PROEFLES</h1>
+                    </div>
+                    <div class="cards_img">
+                        <img src="static/img/card_1.jpg" alt="None">
+                    </div>
+                </div>
+                <div class="card center_img">
+                    <div class="cards_title">
+                        <h1>1 BEURT</h1>
+                    </div>
+                    <div class="cards_img">
+                        <img src="static/img/card_2.jpg" alt="None">
+                    </div>
+                </div>
+                <div class="card center_img">
+                    <div class="cards_title">
+                        <h1>10 BEURTEN</h1>
+                    </div>
+                    <div class="cards_img">
+                        <img src="static/img/card_3.jpg" alt="None">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </body>
+</html>
